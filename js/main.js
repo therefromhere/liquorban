492d1173c595bc1fc5f73659d8fe257d886b5b21
John Carter <john@therefromhere.org>
2016-12-22 21:57:50 +1300

    Added tooltip

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-var mymap = L.map('mapid').setView([-36.87, 174.77], 10);
+var mymap = L.map('mapid').setView([-36.87, 174.77], 12);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
-var liquorban = L.geoJson.ajax("geojson/auckland_liquor_ban.geojson").addTo(mymap);
+function popUp(f,l) {
+    var out = [];
+    var name = f.properties.BYLAWTITLE;
+    var hours = f.properties.HOURSOFOPE;
+    var label = "<b>" + name + "</b><br />" + hours;
+
+    l.bindPopup(label);
+}
+
+var liquorban = L.geoJson.ajax("geojson/auckland_liquor_ban.geojson", {
+    onEachFeature: popUp
+}).addTo(mymap);
