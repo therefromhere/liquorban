581ac8ca7d7260eef7791a4fdf33703d291068bd
John Carter <john@therefromhere.org>
2016-12-22 18:06:45 +1300

    load liquor ban GeoJSON via ajax

--- js/main.js
+++ js/main.js
@@ -3,3 +3,5 @@ var mymap = L.map('mapid').setView([-36.87, 174.77], 10);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
+
+var liquorban = L.geoJson.ajax("geojson/auckland_liquor_ban.geojson").addTo(mymap);
